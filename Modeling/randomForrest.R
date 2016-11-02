b73eef656cff57824182bf01aa14cf3ec38e458f
koehnden <dkoehn@hotmail.de>
2016-11-02 17:02:41 +0100

    bug fixed (paraellization to be implemented)

--- Modeling/randomForrest.R
+++ Modeling/randomForrest.R
@@ -1,16 +1,28 @@
+############################### Random Forrest  #############################################################
+# This script tunes the parameter of a Random Forrest using GridSearch and cross-validation 
+# Depending on the parameter set this takes quite a while
 library(randomForest)
-library(caret)
+library(foreach) # to paraellize for loops (to be implemented)
+library(doParallel) # to register the cores as a cluster 
+
+number_of_cores <- 4 # change the 4 to your number of CPU cores
+cl<-makeCluster(number_of_cores) 
+registerDoParallel(cl) # to register DOParaellel
 
 ########## perform repeated nested cv
 # set cv parameter
-t_outer <- 5 # repetitions on the outer loop
-k_outer <- 5 # fold of the outer cv loop
-t_inner <- 5 # repetition on inner loop (here caret does it)
+t_outer <- 5 # repetitions on the outer loop (not implemented)
+k_outer <- 5 # fold of the outer cv loop (not yet implemented)
+t_inner <- 5 # repetition on inner loop (not implemented)
 k_inner <- 5 # folds on the inner cv loop
 
 # create Grid for GridSearch to tune hyperparameter (here just lambda)
-rfGrid <-  expand.grid(mtry = seq(10,50,10), ntree = c(300,600,900), maxnodes = c(5,10,15,20))  
-# samplesize could 
+# Parameters: mtry: variable considered at each split
+#             ntree: number of tree to grown 
+#             maxnodes: longest path of a single tree
+#             samplesize: size of the bagging bootstrap sample (not yet inspected)
+rfGrid <-  expand.grid(mtry = seq(10,50,10), ntree = 500, maxnodes = seq(5,25,5))  
+# samplesize could be inspected as well
 numOfParameter <- ncol(rfGrid)  
 
 # create a matrix to with the GridSearch parameters and their RMSE
@@ -24,12 +36,11 @@ colnames(best_parameter_temp) <- c("mtry","ntree","max_nodes")
 # draw random integers for the k-folds
 folds_inner <- sample(rep(1:k_inner, length.out = nrow(train)))
 # start crossvalidation loop
-for(k_1 in 1:k_inner){
+for (k_1 in 1:k_inner) {
   # split into training and validation set
-  indexValidation <- which(folds_outer == k_1)
+  indexValidation <- which(folds_inner == k_1)
   training <- train[-indexValidation,]
   validation <- train[indexValidation,]
-  
   # start GridSearch loop 
   for(i in 1:nrow(rfGrid)){
     # fit the randomForrest
@@ -53,7 +64,6 @@ for(k_1 in 1:k_inner){
                                      best_results$ntree,
                                      best_results$max_nodes
                                      ) 
-  
   # print temporary results
   cat("\n")
   cat("RMSE:",   best_results$rmse,
@@ -64,6 +74,8 @@ for(k_1 in 1:k_inner){
   )
 }#end inner cv
 
+
+stopCluster(cl) # stop the cluster  
 
 
 
