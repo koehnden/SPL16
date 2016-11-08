d13aced2b1e565c67f9c49c31a60269bee312ee7
koehnden <dkoehn@hotmail.de>
2016-11-08 19:49:48 +0100

    added results

--- Modeling/xgb_nrounds_tune.R
+++ Modeling/xgb_nrounds_tune.R
@@ -3,12 +3,13 @@ library(ggplot2)
 library(xgboost)
 
 source("load_ames_data.R")
-source("utils/quick_preprocessing.R")
+source("utils/quick_preprocessing.R") # naive preprecessing
 
 # Function to find the optimal number of iteration (nrounds) for xgboost. Function evaluates each number of nround using
 # k-fold cross-validation and stop after n iteration (specified by early_stop) without improvements. This is done 
-# repeated time. In the end the final number of iteration for further tuning holding can be determine be taking the mean or
-# median of the t-results
+# repeated times. In the end the final number of iteration for further tuning holding can be determine be taking the mean or
+# median of the t-results. Results are save as csv-files under "Modeling/Results/xgboost/norunds/xgb_nrounds_train_t_.csv"
+# make sure that your working directory is set to SPL16!!!
 # 
 # input: xgb_parameters - a list of arbitrary parameters that are holded fixed while experimenting with nrounds 
 #        feature_matrix - the training data without labels!!
@@ -21,7 +22,7 @@ source("utils/quick_preprocessing.R")
 # output: a vector of the repetition length containing the best nround in each repetition
 tune_nrounds <- function(xgb_parameters, feature_matrix, labels, max_nrounds = 1000, k = 10, repetitions = 10, early_stop = 20){
   optimal_rounds <- rep(0,repetitions)
-  result_path <- "Modeling/Results/xgboost/norunds/xgb_nrounds_train"
+  result_path <- "Modeling/Results/xgboost/nrounds/xgb_nrounds_train"
   for(t in 1:repetitions){
     # cross-validate xgboost to get the accurate measure of error
     xgb_cv = xgb.cv(params = xgb_parameters, # previously specified parameters
@@ -37,12 +38,9 @@ tune_nrounds <- function(xgb_parameters, feature_matrix, labels, max_nrounds = 1
     )
     # get the optimal number of nrounds
     optimal_rounds[t] <- nrow(xgb_cv$dt) - early_stop
+    # save all training results as csv file xgb_nrounds_train_repetition.csv
+    write.csv(as.data.frame(xgb_cv$dt), file = paste(result_path, t,".csv", sep="_"))
   }#end repeatitions
-  # print average result and std
-  cat("\n")
-  cat("Average optimal round is:", mean(optimal_rounds), "with a Standard Dev of:", sd(optimal_rounds))
-  # save all training results as csv file xgb_nrounds_train_repetition.csv
-  write.csv(xgb_cv$dt, file = paste(result_path, t,".csv", sep="_"))
   # return whole result
   return(optimal_rounds)
 }
@@ -63,11 +61,12 @@ xgb_parameters = list(
   colsample_bytree=0.8,    # fraction of variable to consider per tree (similar to mtry in rf)
   subsample = 0.8          # fraction of the whole sample that the bootstrap sample should consist of (like ntree in rf)
 )
-# takes ~5min with repetition = 10
+# takes ~30min with repetition = 100
 optimal_nrounds <- tune_nrounds(xgb_parameters=xgb_parameters, feature_matrix=train, labels = Y, repetitions = 100)  
-median(optimal_nrounds)
-mean(optimal_nrounds)
-sd(optimal_nrounds)
+median(optimal_nrounds) # 109.5 
+mean(optimal_nrounds)   # 116.25
+sd(optimal_nrounds) # 28.92
+final_rounds = mean(optimal_nrounds) + sd(optimal_nrounds)  # ~145
 
 
 
@@ -92,4 +91,4 @@ ggplot() +
   geom_line(aes(nrounds, test.rmse.mean, colour="red"), res_cv) +  
   geom_line(aes(nrounds, train.rmse.mean, colour="blue"), res_cv) +
   ggtitle("Training vs. Test Error (Number of Iterations)") +
-  xlab("nrounds") + ylab("RMSE") + 
+  xlab("nrounds") + ylab("RMSE") 
