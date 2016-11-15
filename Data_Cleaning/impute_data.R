87af7d02fc91f3d1865ee6ca77b40b2175bb4bd8
koehnden <dkoehn@hotmail.de>
2016-11-15 20:12:47 +0100

    mice imputation update

--- Data_Cleaning/impute_data.R
+++ Data_Cleaning/impute_data.R
@@ -1,6 +1,9 @@
 ################### Quick Data Cleaning ####################################
 # functions only work if Hmisc package is installed
 library(Hmisc) # for imputation function
+library(mice)
+library(VIM)
+#library(xtabel)
 
 # function to compute the mode of a variable (used to impute categorical variables)
 # input: x - a variable 
@@ -14,7 +17,7 @@ Mode <- function(x) {
 # function to impute a variable (numerical with median and categoricals with mode)
 # input: x - a variable 
 # output: x with imputed NAs by either the mean or the mode
-impute_variable <- function(x) {
+median_mode_impute <- function(x) {
   if(any(is.na(x))){
     if(is.numeric(x) & !is.factor(x) ){
       x <-  impute(x, fun = mean)
@@ -30,15 +33,31 @@ impute_variable <- function(x) {
 # function to impute a whole feature matrix (numerical with median and categoricals with mode)
 # input: X - a data.frame  
 # output: data.frame X with imputed NAs by either the mean or the mode
-quick_imputation <- function(X){
+naive_imputation <- function(X){
   temp <- X
   # Four Variables namely "Allez", "PoolQC" ,"Fence", "MiscFeature" have over 2000 missings --> we drop them
   temp <- temp[ , !names(temp) %in% c("Alley", "PoolQC" ,"Fence", "MiscFeature")] 
   # apply function on all variable using lappy 
   temp <- data.frame(temp, stringsAsFactors = FALSE)
-  temp <- as.data.frame(lapply(temp, impute_variable))
+  temp <- as.data.frame(lapply(temp, median_mode_impute)) 
   return(temp)
 }
+# usage
+X_imputed <- naive_imputation(X_com)
+
+# function to impute a whole feature matrix using the mice package defaults: numerical with pmm
+# and categoricals with log/poly reg, This take a while. The imputed data set can be found 
+# input: X - a data.frame  
+# output: data.frame X with imputed NAs by either the mean or the mode
+mice_imputation <- function(x){
+  temp <- mice(x, MaxNWts=5000) # MaxNWt from the nnet package
+  temp <- complete(temp,1)
+  return(temp)
+}
+
+#result_path <- "Data/ames_imputed_mice.csv"
+#X_imputed_mice <- mice_imputation(X_com)
+#write.csv(X_imputed_mice,result_path)
 
 ## usage
 # apply quick_impute function to the complete feature matrix X_com
--- utils/quick_preprocessing.R
+++ utils/quick_preprocessing.R
@@ -5,11 +5,11 @@ source("load_ames_data.R")
 # input: X_com - complete feature matrix with feature in train and test set (return by load_ames_data.R)
 #        y - vector of labels in the training set
 # output: a preprocessed version of the training set including the labels
-quick_preprocessing <- function(X_com,y){
+naive_preprocessing <- function(X_com,y){
   source("Data_Cleaning/convert_categoricals.R")
   source("Data_Cleaning/impute_data.R")
   source("Feature_Selection/delete_nearzero_variables.R") # Put ouT X_com as the cleaned Feature Matrix
-  X_imputed <- quick_imputation(X_com)
+  X_imputed <- naive_imputation(X_com)
   X_encoded <- data.frame(lapply(X_imputed, cat_to_dummy))
   X_com <- delect_nz_variable(X_encoded)
   
@@ -19,5 +19,23 @@ quick_preprocessing <- function(X_com,y){
 }
 
 ## example
-train <- quick_preprocessing(X_com,y)
+train <- naive_preprocessing(X_com,y)
 
+# applies for sophisticated preprocessing done in cat_to_dummies and delete_nz_variables in one step
+# input: X_imputed - complete feature matrix with feature in train and test set where NAs are already imputed by some method
+#        y - vector of labels in the training set
+# output: a preprocessed version of the training set including the labels
+preprocessing <- function(X_imputed,y){
+  source("Data_Cleaning/convert_categoricals.R")
+  source("Feature_Selection/delete_nearzero_variables.R") # Put ouT X_com as the cleaned Feature Matrix
+  X_encoded <- data.frame(lapply(X_imputed, cat_to_dummy))
+  X_com <- delect_nz_variable(X_encoded)
+  
+  # remerge train data 
+  train <- cbind(X_com[1:length(y),],y)
+  return(train)
+}
+
+## example
+#X_imputed <- read.csv("Data/ames_imputed.csv")
+#train <- preprocessing(X_imputed,y)
