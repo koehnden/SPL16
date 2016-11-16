7d5f915efcbd8caf13134fddc1ff91131c5b67fb
koehnden <dkoehn@hotmail.de>
2016-11-16 15:20:44 +0100

    nested crossvalidtion for stoachstic gradient boosting

--- Modeling/gradBoosting.R
+++ Modeling/gradBoosting.R
@@ -22,22 +22,21 @@ result_path <- "Modeling/Results/xgboost/tree_specific/xgb_treespecific_train"
 
 ########## perform repeated nested cv
 # set cv parameter
-t_outer <- 5 # repetitions on the outer loop (not implemented)
-k_outer <- 5 # fold of the outer cv loop (not yet implemented)
-t_inner <- 5 # repetition on inner loop (not implemented)
-k_inner <- 5 # folds on the inner cv loop
+t_outer <- 5   # repetitions on the outer loop (not implemented)
+k_outer <- 10  # fold of the outer cv loop 
+k_inner <- 5   # folds on the inner cv loop
 
 # create Grid for GridSearch to tune hyperparameter 
 # Tree specific Parameters: maxnodes: longest path of a single tree (decreased performance)
 #                           colsample_bytree: variable considered at each split
 #                           subsample: size of the bagging bootstrap sample
 
-nrounds_fixed <- 200 # number of trees: tune_nrounds gave ~150 but with a sd of 50 --> I set it to 200 
-eta_fixed <- 0.1 # learning rate (fixed for now)
-treeSpecificGrid <-  expand.grid(max_depth = seq(4,10,2), 
-                                 gamma = seq(0,0.2,0.05), 
-                                 subsample = seq(0.6,1,0.2), 
-                                 colsample_bytree = seq(0.6,1,0.2)
+nrounds_fixed <- 300 # number of trees: set depending on the learning rate (lower learning rate --> higher nrounds) 
+eta_fixed <- 0.025 # learning rate (fixed for now)
+treeSpecificGrid <-  expand.grid(max_depth = seq(2,10,2), 
+                                 gamma = 0, # gamma seems to be not be crucial (we do not tune it)
+                                 subsample = seq(0.4,1,0.2), 
+                                 colsample_bytree = seq(0.1,0.85,0.15)
                                  )
 # samplesize could be inspected as well
 numOfParameter <- ncol(treeSpecificGrid)  
@@ -47,39 +46,55 @@ parameter_names <- c("max_depth",
                      "subsample",
                      "colsample_bytree"
 )
-tuning_results <- cbind(rep(0,nrow(treeSpecificGrid)),treeSpecificGrid)
-colnames(tuning_results) <- c("rmse",parameter_names)
-
-
 # set up empty matrices to be filled with rmse and best_paramer (here only lambda)
 
 
 ### start nested repeated nested cv loop
 ## Repetition outer loop
 # create empty vector/matrix to save best results
-rmse_temp <- matrix(0, nrow = k_inner, ncol = t_inner)
-best_parameter <- matrix(0, nrow = k_inner * t_inner, ncol = numOfParameter) # every 10th row represent a new repetition 
-colnames(best_parameter) <- parameter_names
+rmse_temp <- matrix(0, nrow = k_inner, ncol = k_outer)
+kbest <- 5 # how many models to be run in the outer loop
+results_outer <- matrix(0, nrow = k_outer, ncol = kbest)
+colnames(result_outer) <- c("1st","2nd","3rd","4th","5th")
+best_parameter <- matrix(0, nrow = k_inner, ncol = numOfParameter + 1) 
+colnames(best_parameter) <- c("rmse",parameter_names)
+result_list <- lapply(seq_len(k_outer), function(X) best_parameter)
 
 # start nested cv loop
-for(t_1 in 1:t_inner){
-  cat("\n")
-  cat("start inner repetition", t_1, "out of", t_inner)
+for(k_1 in 1:k_outer){
   # draw random integers for the k-folds
-  folds_inner <- sample(rep(1:k_inner, length.out = nrow(train)))
+  folds_outer <- sample(rep(1:k_outer, length.out = nrow(train)))
+  # split into training and validation set
+  indexValidation_outer <- which(folds_outer == k_1)
+  training_outer <- train[-indexValidation_outer,]
+  y_training_outer <- y[-indexValidation_outer]
+  validation_outer <- train[indexValidation_outer,]
+  y_validation_outer <- y[indexValidation_outer]
+  # convert for data into a format xgb.train can handle for the outer training
+  dtrain_outer <- xgb.DMatrix(data = as.matrix(training_outer), label=y_training_outer)
+  dvalidation_outer <- xgb.DMatrix(data = as.matrix(validation_outer), label=y_validation_outer)
+  cat("\n")
+  cat("start outer nested cv loop", k_1, "out of", k_outer)
+  # draw random integers for the k-folds
+  folds_outer <- sample(rep(1:k_outer, length.out = nrow(train)))
   
+  # create empty vector/matrix to save best results
+  tuning_results <- cbind(rep(0,nrow(treeSpecificGrid)),treeSpecificGrid)
+  colnames(tuning_results) <- c("rmse",parameter_names)
   # start crossvalidation loop
-  for(k_1 in 1:k_inner){
+  for(k_2 in 1:k_inner){
+    # draw random integers for the k-folds
+    folds_inner <- sample(rep(1:k_inner, length.out = nrow(training_outer)))
     # split into training and validation set
-    indexValidation <- which(folds_inner == k_1)
-    training <- train[-indexValidation,]
-    y_training <- y[-indexValidation]
-    validation <- train[indexValidation,]
-    y_validation <- y[indexValidation]
+    indexValidation_inner <- which(folds_inner == k_2)
+    training_inner <- training_outer[-indexValidation_inner,]
+    y_training_inner <- y_training_outer[-indexValidation_inner]
+    validation_inner <- training_outer[indexValidation_inner,]
+    y_validation_inner <- y_training_outer[indexValidation_inner]
     
     # convert for data into a format xgb.train can handle
-    dtrain <- xgb.DMatrix(data = as.matrix(training), label=y_training)
-    dvalidation <- xgb.DMatrix(data = as.matrix(validation), label=y_validation)
+    dtrain_inner <- xgb.DMatrix(data = as.matrix(training_inner), label=y_training_inner)
+    dvalidation_inner <- xgb.DMatrix(data = as.matrix(validation_inner), label=y_validation_inner)
     # start GridSearch loop 
     for(i in 1:nrow(treeSpecificGrid)){
       # determine arbitrary xgboost parameters in a list
@@ -89,52 +104,70 @@ for(t_1 in 1:t_inner){
         gamma = treeSpecificGrid$gamma[i],                          # minimal improvement per iteration
         colsample_bytree = treeSpecificGrid$colsample_bytree[i],    # fraction of variable to consider per tree (similar to mtry in rf)
         subsample = treeSpecificGrid$subsample[i],                  # fraction of the whole sample that the bootstrap sample should consist of 
-        eval_metric = "rmse",                                      # error metric
+        eval_metric = "rmse",                                       # error metric
         maximize = FALSE
         )
-      # fit the randomForrest
-      xgbFit <- xgb.train(params = xgb_paramters,  # list of parameter previously specified
-                          data =  dtrain,
+      # fit the xgboost
+      xgbFit_inner <- xgb.train(params = xgb_paramters,  # list of parameter previously specified
+                          data =  dtrain_inner,
                           booster = "gbtree",
                           nround = nrounds_fixed,    #number of trees (set accordingly to tune_nrounds function) 
                           verbose = TRUE,
                           objective = "reg:linear"
                           )
       # predict SalePrice
-      yhat <- predict(xgbFit, newdata = dvalidation)
+      yhat_inner <- predict(xgbFit_inner, newdata = dvalidation_inner)
       # fill the first column of this matrix with the rmse results (of the log outputs)
-      validation_error <- rmse_log(y_validation,yhat)
-      tuning_results[i,1] <- validation_error
+      validation_error_inner <- rmse_log(y_validation_inner, yhat_inner)
+      tuning_results[i,1] <- validation_error_inner
       # save all training results as csv file (fold_k_reptetion_t)
-      write.csv(tuning_results, file = paste(result_path, k_1,t_1,".csv", sep="_"))
-      cat("\n")
-      cat(validation_error)
+      write.csv(tuning_results, file = paste(result_path, k_2,k_1,".csv", sep="_"))
     }#end GridSearch
-    # find the index of the best model 
-    idx_best <- which(tuning_results$rmse == min(tuning_results$rmse))
+    # find the index of the 5 best models with the smallest rmse 
+    idx_kbest <- order(tuning_results$rmse)[1:5]
     # get rmse and the parameter of the best model
-    best_results <- tuning_results[idx_best,]
+    best_results <- tuning_results[idx_kbest,]
     # save best results
-    rmse_temp[k_1,t_1] <- best_results$rmse # best rmse
-    best_parameter[k_1*t_1,] <- cbind(best_results$max_depth,
-                                      best_results$gamma,
-                                      best_results$subsample,
-                                      best_results$colsample_bytree
-    ) 
-    # print temporary results
+    rmse_temp[k_2,k_1] <- best_results$rmse[1] # best rmse
+    # print temporary results of tuning
     cat("\n")
-    cat("Best RMSE:",  best_results$rmse,
-        "eta:",   best_results$eta,
-        "nrounds:",  best_results$nrounds,
-        "max_depth:", best_results$max_depth,
-        "inner fold:", k_1, "out of", k_inner
+    cat("Best RMSE:",  best_results$rmse[1],
+        "max_depth:",  best_results$max_depth[1],
+        "gamma:",      best_results$gamma[1],
+        "colsample_bytree:", best_results$colsample_bytree[1],
+        "subsample:", best_results$subsample[1],
+        "inner fold:", k_2, "out of", k_inner[1]
     )
   }#end inner cv
-}#end inner repetition
-
-# get average prediction error per repetition
-colMeans(rmse_temp)
-table(best_parameter[,1])
-table(best_parameter[,2])
-table(best_parameter[,3])
-table(best_parameter[,4])
+  # fill result list with the best parameter 
+  result_list[[k_1]] <- best_results
+  # start training with k-best parameter set using the outer training data
+  for(model in 1:nrow(best_results)){
+    # set xgboost parameters to the k best parameter set
+    xgb_paramters = list(                                              
+      eta = eta_fixed,                                            # learning rate                                                                
+      max.depth = best_results$max_depth[model],                  # max nodes of a tree                                                       
+      gamma = best_results$gamma[model],                          # minimal improvement per iteration
+      colsample_bytree = best_results$colsample_bytree[model],    # fraction of variable to consider per tree (similar to mtry in rf)
+      subsample = best_results$subsample[model],                  # fraction of the whole sample that the bootstrap sample should consist of 
+      eval_metric = "rmse",                                       # error metric
+      maximize = FALSE
+    )
+    # fit the xgboost for outer training
+    xgbFit_outer <- xgb.train(params = xgb_paramters,  # list of parameter previously specified
+                              data =  dtrain_outer,
+                              booster = "gbtree",
+                              nround = nrounds_fixed,    #number of trees (set accordingly to tune_nrounds function) 
+                              verbose = TRUE,
+                              objective = "reg:linear"
+    )
+    # predict SalePrice
+    yhat_outer <- predict(xgbFit_outer, newdata = dvalidation_outer)
+    # fill the first column of this matrix with the rmse results (of the log outputs)
+    validation_error_outer <- rmse_log(y_validation_outer, yhat_outer)
+    write.csv(results_outer, file = paste(result_path,k_2,model,"bestModels.csv", sep="_"))
+    result_list[[k_1]][model,1] <- validation_error_outer
+    # save all training results as csv file (fold_k_reptetion_t)
+  }# end outer training
+}#end outer cv loop
+print(result_list) 
