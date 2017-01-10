05f43ad3dfb2d3ba0d00ffeb9d5f6f7b4cdc88a1
koehnden <dkoehn@hotmail.de>
2017-01-10 16:49:28 +0100

    deleting old file

--- Quantlet/data_processing/delete_nearzero_variables.R
+++
@@ -1,7 +0,0 @@
-################### delete near zero variance variable #######################
-library(caret)
-
-delect_nz_variable = function(X) {
-    near_zero_variance = nearZeroVar(X)
-    return(X[, -near_zero_variance])
-}
